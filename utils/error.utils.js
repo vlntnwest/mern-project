97982fb1771eed41073752e0e6caebb7fe776370
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2024-07-02 18:56:22 +0200

    Adding utils

---
+++ utils/error.utils.js
@@ -0,0 +1,27 @@
+module.exports.signUpErrors = (err) => {
+  let errors = { pseudo: "", email: "", passord: "" };
+  if (err.message.includes("pseudo"))
+    errors.pseudo = "Pseudo incorrect ou déjà pris";
+  if (err.message.includes("email"))
+    errors.email = "Email incorrect ou déjà pris";
+  if (err.message.includes("password"))
+    errors.password = "Le mot de passe doit faire 6 caractères minimum";
+
+  if (err.code === 11000 && Object.keys(err.keyValue)["pseudo"])
+    errors.pseudo = "Ce pseudo est déjà utilisé";
+
+  if (err.code === 11000 && Object.keys(err.keyValue)["email"])
+    errors.email = "Cet email est déjà utilisé";
+
+  return errors;
+};
+
+module.exports.signInErrors = (err) => {
+  let errors = { email: "", passord: "" };
+
+  if (err.message.includes("email")) errors.email = "Email inconnu";
+  if (err.message.includes("password"))
+    errors.password = "Le mot de passe ne correspond pas";
+
+  return errors;
+};
